a7552176dc332d813b2bc53500ebeeb12515697f
akchirpy <akchirpy@gmail.com>
2025-12-10 13:24:44 -0600

    Update vite.config.js

--- dashboard-react/vite.config.js
+++ dashboard-react/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/web-push-service/dashboard-react/',
+  base: '/web-push-service/',
   build: {
     outDir: 'dist',
   },
